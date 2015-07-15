60db23b7c31dd5ace6a27708cd6a85ffe5f822f2
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-15 22:09:03 +0200

    initial tests

---
+++ test.js
@@ -0,0 +1,64 @@
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+
+describe('generator', function () {
+  beforeEach(function (cb) {
+    var deps = ['../app'];
+
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      this.generator = helpers.createGenerator('nm:app', deps, null, {skipInstall: true});
+      cb();
+    }.bind(this));
+  });
+
+  it('generates expected files', function (cb) {
+
+    var expected = [
+      '.editorconfig',
+      '.gitignore',
+      '.npmignore',
+      '.travis.yml',
+      'package.json',
+      'index.js',
+      'test.js',
+      'readme.md',
+    ];
+
+    helpers.mockPrompt(this.generator, {
+      moduleName: 'test',
+      githubUsername: 'test',
+      website: 'test.com',
+      cli: false
+    });
+
+    this.generator.run(function () {
+      assert.file(expected);
+      assert.noFile('cli.js');
+      cb();
+    });
+  });
+
+  it('CLI option', function (cb) {
+    helpers.mockPrompt(this.generator, {
+      moduleName: 'test',
+      githubUsername: 'test',
+      website: 'test.com',
+      cli: true
+    });
+
+    this.generator.run(function () {
+      assert.file('cli.js');
+      assert.fileContent('package.json', /"bin":/);
+      assert.fileContent('package.json', /"bin": "cli.js"/);
+      assert.fileContent('package.json', /"meow"/);
+      cb();
+    });
+  });
+});

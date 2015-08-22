de2aeda30587c9859cc8be548de3a7046a07073a
Jorge Bucaran <jbucaran@me.com>
2015-08-23 00:09:27 +0900

    Switch template tests to tape with full coverage & tweaks.
    + Tape tests with coverage report and babel
    + Lint with ESLint
    + Lint on precommit
    + Remove semicolons
    + Add default URL based in user's GitHub by default
    + Simplify generator's test
    + Small tweaks to README

--- app/index.js
+++ app/index.js
@@ -1,86 +1,103 @@
-'use strict';
-
-var yeoman = require('yeoman-generator');
-var normalizeUrl = require('normalize-url');
-var humanizeUrl = require('humanize-url');
-var _s = require('underscore.string');
+const yeoman = require('yeoman-generator');
+const normalizeUrl = require('normalize-url');
+const humanizeUrl = require('humanize-url');
+const _s = require('underscore.string');
 
 module.exports = yeoman.generators.Base.extend({
   init: function () {
-    var cb = this.async();
+    const done = this.async()
 
-    this.prompt([{
+    this.prompt([
+    {
       name: 'moduleName',
       message: 'name:',
       default: this.appname.replace(/\s/g, '-'),
       filter: function (val) {
-        return _s.slugify(val);
+        return _s.slugify(val)
       }
-    }, {
+    },
+    {
       name: 'moduleDesc',
       message: 'description:'
-    }, {
+    },
+    {
       name: 'moduleKeywords',
       message: 'keywords:'
-    }, {
+    },
+    {
       name: 'moduleVersion',
       message: 'version:',
       store: true,
       default: '0.0.0',
-    }, {
+    },
+    {
       name: 'moduleLicense',
       message: 'license:',
       store: true,
       default: 'MIT',
-    }, {
+    },
+    {
       name: 'githubUsername',
       message: 'github username:',
       store: true,
       validate: function (val) {
-        return val.length > 0 ? true : 'You have to provide a username';
+        return val.length > 0 ? true : 'You have to provide a username'
       }
-    }, {
+    },
+    {
+      type: "confirm",
+      name: "changelog",
+      message: "changelog:",
+      store: true,
+      default: true
+    },
+    {
       name: 'website',
       message: 'website:',
       store: true,
-      validate: function (val) {
-        return val.length > 0 ? true : 'You have to provide a website URL';
+      default: function (props) {
+        return "http://github.com/" + props.githubUsername
       },
       filter: function (val) {
-        return normalizeUrl(val);
+        return normalizeUrl(val)
       }
-    }], function (props) {
-      var tpl = {
-        moduleName: props.moduleName,
-        moduleDesc: props.moduleDesc,
-        moduleKeywords: (props.moduleKeywords || '').trim().split(',').map(function(i) { return (i || '').trim(); }),
-        moduleVersion: props.moduleVersion,
-        moduleLicense: props.moduleLicense,
-        camelModuleName: _s.camelize(props.moduleName),
-        githubUsername: props.githubUsername,
-        name: this.user.git.name(),
-        email: this.user.git.email(),
-        website: props.website,
-        humanizedWebsite: humanizeUrl(props.website)
-      };
+    }],
+    function (props) {
+      this.moduleName = props.moduleName
+      this.moduleDesc = props.moduleDesc
+      this.moduleVersion = props.moduleVersion
+      this.moduleLicense = props.moduleLicense
+      this.camelModuleName = _s.camelize(props.moduleName)
+      this.githubUsername = props.githubUsername
+      this.name = this.user.git.name()
+      this.email = this.user.git.email()
+      this.website = props.website
+      this.humanizedWebsite = humanizeUrl(props.website)
+      this.moduleKeywords = (props.moduleKeywords || '')
+        .trim()
+        .split(',')
+        .map(function(i) {
+          return (i || '').trim()
+        })
+      this.hasChangelog = props.changelog
 
-      var cpTpl = function (from, to) {
-        this.fs.copyTpl(this.templatePath(from), this.destinationPath(to), tpl);
-      }.bind(this);
+      this.template('package.json')
+      this.template('README.md')
+      if (this.hasChangelog) {
+        this.template("CHANGELOG.md")
+      }
+      this.template('index.js',      'src/index.js')
+      this.template('test.js',       'test/index.js')
+      this.template('editorconfig',  '.editorconfig')
+      this.template('gitignore',     '.gitignore')
+      this.template('npmignore',     '.npmignore')
+      this.template('travis.yml',    '.travis.yml')
+      this.template('eslintrc',      '.eslintrc')
 
-      cpTpl('_index.js',     'index.js');
-      cpTpl('_package.json', 'package.json');
-      cpTpl('_README.md',     'README.md');
-      cpTpl('_test.js',      'test.js');
-      cpTpl('editorconfig',  '.editorconfig');
-      cpTpl('gitignore',     '.gitignore');
-      cpTpl('npmignore',     '.npmignore');
-      cpTpl('travis.yml',    '.travis.yml');
-
-      cb();
-    }.bind(this));
+      done()
+    }.bind(this))
   },
   install: function () {
-    this.installDependencies({bower: false});
+    this.installDependencies({ bower: false })
   }
-});
+})

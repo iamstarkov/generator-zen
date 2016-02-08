8fbc8f470c00bc431f5f81bf4c6f05115fb7acd0
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-08 12:41:55 +0100

    move schroedinger log entirely into separate file

--- app/index.js
+++ app/index.js
@@ -76,10 +76,6 @@ module.exports = yeoman.Base.extend({
 
     if (shouldAskAll && shouldSkipAll) {
       this.log(cat);
-      this.log('Congratulations! You just catched Schrödinger\'s cat.');
-      this.log('You have chosen to ask both "all" and "minimum" questions.');
-      this.log('P.S. Please be clear in your intentions\n');
-      this.log('Sincerely yours, \nSchrödinger\'s cat.\n');
       return;
     }
 

6073cee2994f6d0904b4b0ddc210f2eeed368d56
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-01 00:42:27 +0100

    write tests

--- test.js
+++ test.js
@@ -4,17 +4,18 @@
 var path = require('path');
 var helpers = require('yeoman-test');
 var assert = require('yeoman-assert');
+var R = require('ramda');
+
+var defaults = {
+  moduleName: 'module',
+  githubUsername: 'username',
+  website: 'test.com',
+  moduleDesc: 'Your awsm module!',
+};
 
 it('generates expected files', function (done) {
   helpers.run(path.join(__dirname, './app'))
-    .withOptions({ skipInstall: true })
-    .withPrompts({
-      moduleName: 'module',
-      githubUsername: 'username',
-      website: 'test.com',
-      moduleDesc: 'Your awsm module!',
-      moduleTest: 'tape',
-    })
+    .withPrompts(R.merge(defaults, { moduleTest: 'tape' }))
     .on('end', function () {
       assert.file([
         '.editorconfig',
@@ -28,6 +29,44 @@ it('generates expected files', function (done) {
         '.eslintrc.json',
         '.git',
       ]);
+      done();
+    });
+});
+
+it.only('generates all needed for mocha', function (done) {
+  helpers.run(path.join(__dirname, './app'))
+    .withPrompts(R.merge(defaults, { moduleTest: 'mocha' }))
+    .on('end', function () {
+      assert.jsonFileContent('package.json', {
+        scripts: { test: 'mocha --require babel-register' },
+      });
+      assert.fileContent('package.json', /assert/);
+      assert.fileContent('package.json', /mocha/);
+      done();
+    });
+});
+
+it('generates all needed for tape', function (done) {
+  helpers.run(path.join(__dirname, './app'))
+    .withPrompts(R.merge(defaults, { moduleTest: 'tape' }))
+    .on('end', function () {
+      assert.jsonFileContent('package.json', {
+        scripts: { test: 'tape test.js --require babel-register | tap-spec' },
+      });
+      assert.fileContent('package.json', /tap-spec/);
+      assert.fileContent('package.json', /tape/);
+      done();
+    });
+});
+
+it('generates all needed for ava', function (done) {
+  helpers.run(path.join(__dirname, './app'))
+    .withPrompts(R.merge(defaults, { moduleTest: 'ava' }))
+    .on('end', function () {
+      assert.jsonFileContent('package.json', {
+        scripts: { test: 'tape test.js --require babel-register | tap-spec' },
+      });
+      assert.fileContent('package.json', /ava/);
       done();
     });
 });

33f1095c8c8501c3d7219c9f715ee3809655c0c8
andrepolischuk <andre.polischuk@gmail.com>
2016-03-26 22:45:14 +0300

    use ava t.is for assertion of primitive equality

--- app/templates/_test-ava.js
+++ app/templates/_test-ava.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import <%= camelModuleName %> from './index';
 
 test('should <%= camelModuleName %>', (t) =>
-  t.same(<%= camelModuleName %>('unicorns'), 'unicorns'));
+  t.is(<%= camelModuleName %>('unicorns'), 'unicorns'));
 
 test('should <%= camelModuleName %> invalid input', (t) =>
-  t.same(<%= camelModuleName %>(), undefined));
+  t.is(<%= camelModuleName %>(), undefined));

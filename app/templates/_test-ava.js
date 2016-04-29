fa9fb3b5e3cb6b6043aac4a2b0a19c84684d8fda
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-29 15:48:37 +0200

    Update ava tests to be more convenient

--- app/templates/_test-ava.js
+++ app/templates/_test-ava.js
@@ -1,15 +1,18 @@
 import test from 'ava';
 import { <%= camelModuleName %>, <%= camelModuleName %>Async } from './index';
 
-test('basic', t =>
-  t.is(<%= camelModuleName %>('unicorns'), 'unicorns'));
+test('basic', t => t.is(
+  <%= camelModuleName %>('unicorns'),
+  'unicorns'
+));
 
 test('empty input', t => t.throws(() => { <%= camelModuleName %>(); }, TypeError));
 test('invalid input', t => t.throws(() => { <%= camelModuleName %>(2); }, TypeError));
 
 test('async :: basic', async t => t.is(
   await <%= camelModuleName %>Async('unicorns'),
-  'unicorns'));
+  'unicorns'
+));
 
 test('async :: empty input', t => t.throws(<%= camelModuleName %>Async(), TypeError));
 test('async :: invalid input', t => t.throws(<%= camelModuleName %>Async(2), TypeError));
